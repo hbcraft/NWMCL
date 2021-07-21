6a64bde590046317acabc00bc1c20b3b4b14d94a
jyh <admin@hbcraft.cn>
2021-07-21 17:28:30 +0800

    添加ng-zorro前端框架

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NzIconService } from 'ng-zorro-antd/icon';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.less']
 })
 export class AppComponent {
-  title = 'web';
+  isCollapsed = false;
+  theme: 'dark' | 'light' = 'light'
+  get darkThemeIcon() {
+    return `icon-dark${this.theme === 'dark' ? '-active' : ''}`
+  }
+  constructor (
+    private _iconService: NzIconService
+  ) {
+    this._iconService.fetchFromIconfont({
+      scriptUrl: 'https://at.alicdn.com/t/font_2690818_s26qnadqq3n.js'
+    })
+  }
+
+  changeTheme() {
+    this.theme = this.theme === 'dark' ? 'light' : 'dark'
+  }
 }
---
+++ web/src/shared/nz-shared.ts
@@ -0,0 +1,8 @@
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { NzSwitchModule } from 'ng-zorro-antd/switch';
+export const NzModules = [
+  NzLayoutModule,
+  NzMenuModule,
+  NzSwitchModule
+]
